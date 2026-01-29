2d53f45adb910e6a5263b5a81ecbab3e7fa7829b
Thirith Heng <thirithheng@gmail.com>
2026-01-29 23:36:43 +0700

    Init

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-export default {
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'] // separate big libraries
-        }
-      }
-    }
-  }
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/case-flow-igs",
+})
