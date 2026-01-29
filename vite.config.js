296eb6e5653b13fa7de833e9ff6e67c7bef693e5
Thirith Heng <thirithheng@gmail.com>
2026-01-29 23:25:22 +0700

    Init

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'] // separate big libraries
+        }
+      }
+    }
+  }
+}
